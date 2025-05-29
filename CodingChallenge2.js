a2d7856e9b5c92d01ab4030b0006d014eb86d235
unknown <dawarevinay@gmail.com>
2025-05-29 18:00:41 +0530

    Coding challenge2 with if else block

--- CodingChallenge2.js
+++ CodingChallenge2.js
@@ -1,3 +1,4 @@
+// Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95m tall.
 let massMark = 78;
 let heightMark = 1.69;
 let massJohn = 92;
@@ -11,9 +12,24 @@ console.log(BMIJohn);
 let message;
 
 if(BMIMark>BMIJohn){
-    message = `Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`;
+    message = `Mark's BMI (${BMIMark}) is higher than John's BMI (${BMIJohn})!`;
 }else{
-    message = `John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark}) `;
+    message = `John's BMI (${BMIJohn}) is higher than Mark's BMI (${BMIMark}) `;
 }
 console.log(message);  
 
+// Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76m tall.
+massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn = 1.76;
+BMIMark = massMark/(heightMark**2);
+BMIJohn = massJohn/(heightJohn**2);
+console.log(BMIMark, BMIJohn);
+
+if(BMIMark>BMIJohn){
+    message = `Mark's BMI (${BMIMark}) is higher than John's BMI (${BMIJohn})`
+}else{
+    message = `John's BMI (${BMIJohn}) is higher than Mark's BMI (${BMIMark}) `
+}
+console.log(message);
